19be206ab5d0f4fbb310f954c3f6977178ec1b55
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-04-01 21:54:06 -0400

    Post request, update-2

--- src/test/java/POST_Requests.java
+++ src/test/java/POST_Requests.java
@@ -1,9 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.JsonUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class POST_Requests {
     String taskURL = "https://tla-school-api.herokuapp.com/api/school/programs/sdetcourse";
@@ -50,6 +58,82 @@ public class POST_Requests {
     }
 
     //Class Task: Create a json file user.json, add json object compatible with gorest api and read it in test method
+    @Test()
+    void test04(){
+        JSONObject jsonObject = JsonUtils.readBodyFromJsonFile("src\\test\\java\\data\\user.json");
+        System.out.println(jsonObject);
+    }
+
+    //NOTE: POST Requests -------------------------------------------
+
+    @Test(description = "sending a POST Request using JsonObject")
+    void test05(){
+        JSONObject newCourse = new JSONObject();
+        newCourse.put("name", "Running");
+        newCourse.put("duration", "2 months");
+
+        given()
+                //.contentType("application/json")
+                .contentType(ContentType.JSON)
+                .body(newCourse)
+                .when()
+                .post(taskURL)
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
+
+    @Test(description = "Post request with variables + API as a query parameter")
+    void test06(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("firstName", "Mike");
+        jsonObject.put("lastName", "Jhonson");
+        jsonObject.put("email", "mj@test.com");
+        jsonObject.put("batch", 3);
+
+        RestAssured.baseURI = "https://tla-school-api.herokuapp.com/api/school/resources";
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.headers("Content-Type", ContentType.JSON);
+        httpRequest.queryParam("key", "d03e989018msh7f4691c614e87a9p1a8181j");
+        httpRequest.body(jsonObject);
+
+        Response response = httpRequest.request(Method.POST, "/students");
+
+        String responseBody = response.getBody().asString();
+        System.out.println(responseBody);
+
+        int status = response.getStatusCode();
+        System.out.println(status);
+
+        Assert.assertEquals(status, 200);
+    }
+
+    @Test(description = "Post request without variables, without much confusion")
+    void test07(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("firstName", "Mike T");
+        jsonObject.put("lastName", "Jhonson");
+        jsonObject.put("email", "mjT@test.com");
+        jsonObject.put("batch", 3);
+
+        given()
+                .headers("Content-Type", ContentType.JSON)
+                .queryParam("key", "d03e989018msh7f4691c614e87a9p1a8181j")
+                .body(jsonObject)
+                .when()
+                .post("https://tla-school-api.herokuapp.com/api/school/resources/students")
+                .then()
+                .statusCode(200)
+                .log().body();
+
+    }
+
+
+
+
+    //Class Task: Create a POST request for gorest api and create a user
+
+
 
 
 }
